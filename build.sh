72b5f6307736f3f286c44c43358a62654c786ed3
Jonathan Fok kan <jfokkan@inocybe.com>
2015-01-06 15:05:14 -0500

    ADDED aggregator pom.xml

--- build.sh
+++ build.sh
@@ -8,13 +8,28 @@ set -e -x
 
 CURRENT=`pwd`
 
+cat << EOF > pom.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>org.opendaylight.aggregators</groupId>
+  <artifactId>controller-aaa-aggregator</artifactId>
+  <version>1.0.0</version>
+  <packaging>pom</packaging>
+  <modules>
+  	<module>controller</module>
+  	<module>aaa</module>
+  </modules>
+</project>
+EOF
+
 cd src/odlparent
-mvn clean install
+mvn clean install -Dmaven.repo.local=$CURRENT/.m2repo -Dorg.ops4j.pax.url.mvn.localRepository=$CURRENT/.m2repo
 
 cd ${CURRENT}
 cd src/yangtools
-mvn clean install
+mvn clean install -Dmaven.repo.local=$CURRENT/.m2repo -Dorg.ops4j.pax.url.mvn.localRepository=$CURRENT/.m2repo
 
 cd ${CURRENT}
 cd src/controller
-mvn clean install
+mvn clean install -Dmaven.repo.local=$CURRENT/.m2repo -Dorg.ops4j.pax.url.mvn.localRepository=$CURRENT/.m2repo
